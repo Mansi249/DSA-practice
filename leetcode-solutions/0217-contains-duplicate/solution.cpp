bd83c2832d600b01ded69e84149cd1cda41c5fdd
Mansi249 <24bec062@nith.ac.in>
2025-12-24 18:23:22 +0000

    [LeetCode Sync] Runtime - 43 ms (77.22%), Memory - 90.9 MB (74.77%)

---
+++ leetcode-solutions/0217-contains-duplicate/solution.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_set<int> duplicate;
+        for(int x : nums){
+            if(duplicate.count(x)){
+                return true;
+            }
+            duplicate.insert(x);
+        }
+        return false;
+    }
+};
