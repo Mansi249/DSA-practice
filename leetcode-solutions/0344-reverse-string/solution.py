5bd5ece017960a09c3a943678cdc0ca2bb5afc10
Mansi249 <24bec062@nith.ac.in>
2026-03-09 15:19:25 +0000

    [LeetCode Sync] Runtime - 3 ms (45.11%), Memory - 23.4 MB (45.48%)

---
+++ leetcode-solutions/0344-reverse-string/solution.py
@@ -0,0 +1,9 @@
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        right = len(s)-1
+        left = 0
+        while right>left:
+            s[left],s[right] = s[right],s[left]
+            left+=1
+            right-=1
+        
