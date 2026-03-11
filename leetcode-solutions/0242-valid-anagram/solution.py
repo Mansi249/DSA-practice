747c2e2d4327ab4d033a30f95a916fc6bb04f610
Mansi249 <24bec062@nith.ac.in>
2026-03-11 17:25:13 +0000

    [LeetCode Sync] Runtime - 15 ms (44.16%), Memory - 19.4 MB (76.09%)

--- leetcode-solutions/0242-valid-anagram/solution.py
+++ leetcode-solutions/0242-valid-anagram/solution.py
@@ -1,4 +1,11 @@
 from collections import Counter 
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        return Counter(s) == Counter(t)
+        if len(s) != len(t):
+            return False
+        count = Counter(s)
+        for char in t:
+            if count[char]==0:
+                return False
+            count[char] -=1
+        return True
