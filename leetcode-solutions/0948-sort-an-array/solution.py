f5ccd91939335597652b59b05396b422ea3254ba
Mansi249 <24bec062@nith.ac.in>
2026-04-14 20:07:47 +0000

    [LeetCode Sync] Runtime - 619 ms (64.81%), Memory - 28.2 MB (40.12%)

---
+++ leetcode-solutions/0948-sort-an-array/solution.py
@@ -0,0 +1,31 @@
+class Solution:
+    def sortArray(self, nums: List[int]) -> List[int]:
+        return self.mergeSort(nums)
+
+    def mergeSort(self, nums):
+        if len(nums) <= 1:
+            return nums
+
+        mid = len(nums) // 2
+        left = self.mergeSort(nums[:mid])
+        right = self.mergeSort(nums[mid:])
+
+        return self.merge(left, right)
+
+    def merge(self, left, right):
+        result = []
+        i = j = 0
+
+        while i < len(left) and j < len(right):
+            if left[i] < right[j]:
+                result.append(left[i])
+                i += 1
+            else:
+                result.append(right[j])
+                j += 1
+
+        # add remaining elements
+        result.extend(left[i:])
+        result.extend(right[j:])
+
+        return result
