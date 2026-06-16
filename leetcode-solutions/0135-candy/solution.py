4d493bcc887cb4e211af1b66b140b4d57ed7c02b
Mansi249 <24bec062@nith.ac.in>
2026-06-16 20:13:45 +0000

    [LeetCode Sync] Runtime - 15 ms (72.79%), Memory - 21.3 MB (80.15%)

---
+++ leetcode-solutions/0135-candy/solution.py
@@ -0,0 +1,24 @@
+class Solution:
+    def candy(self, ratings: List[int]) -> int:
+        sum =1
+        i =1
+        n = len(ratings)
+        while(i<n):
+            if ratings[i-1] == ratings[i]:
+                sum+=1
+                i+=1
+                continue
+            peak = 1
+            while i<n and ratings[i]>ratings[i-1]:
+                
+                peak+=1
+                sum +=peak
+                i+=1
+            down = 1
+            while i<n and ratings[i]<ratings[i-1]:
+                sum +=down
+                down+=1
+                i+=1
+            if down >peak:
+                sum+= down-peak
+        return sum
