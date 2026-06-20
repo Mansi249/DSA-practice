cf6f6c2d83e69d749de890e5b834a8c803207fa8
Mansi249 <24bec062@nith.ac.in>
2026-06-20 16:18:54 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 19.5 MB (34.88%)

---
+++ leetcode-solutions/0046-permutations/solution.py
@@ -0,0 +1,27 @@
+from typing import List
+
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        result = []
+        path = []
+        used = [False] * len(nums)
+
+        def dfs():
+            if len(path) == len(nums):
+                result.append(path.copy())
+                return
+
+            for i in range(len(nums)):
+                if used[i]:
+                    continue
+
+                path.append(nums[i])
+                used[i] = True
+
+                dfs()
+
+                path.pop()
+                used[i] = False
+
+        dfs()
+        return result
