440ccacf296ebf1b1a4ea91348de3eb3304d7222
Mansi249 <24bec062@nith.ac.in>
2026-06-21 18:44:45 +0000

    [LeetCode Sync] Runtime - 38 ms (81.27%), Memory - 32.9 MB (96.99%)

---
+++ leetcode-solutions/0131-palindrome-partitioning/solution.py
@@ -0,0 +1,22 @@
+class Solution:
+    def partition(self, s: str) -> List[List[str]]:
+        res = []
+        path = []
+        def dfs(i):
+            if i>=len(s):
+                res.append(path.copy())
+                return
+            for j in range(i,len(s)):
+                if isPal(s,i,j):
+                    path.append(s[i:j+1])
+                    dfs(j+1)
+                    path.pop()
+        def isPal(s,l,r):
+            while l<r:
+                if s[l] != s[r]:
+                    return False
+                l +=1
+                r -=1
+            return True
+        dfs(0)
+        return res
