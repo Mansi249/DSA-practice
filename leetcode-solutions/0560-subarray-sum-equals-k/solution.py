1a4a77fccb077232564cba36a48bbedacb395291
Mansi249 <24bec062@nith.ac.in>
2026-07-09 19:26:15 +0000

    [LeetCode Sync] Runtime - 31 ms (72.47%), Memory - 21.8 MB (84.91%)

---
+++ leetcode-solutions/0560-subarray-sum-equals-k/solution.py
@@ -0,0 +1,17 @@
+class Solution:
+    def subarraySum(self, nums: List[int], k: int) -> int:
+        prefix = 0
+        res = 0
+        mp = {0: 1}
+
+        for num in nums:
+            prefix += num
+
+            if prefix - k in mp:
+                res += mp[prefix - k]
+
+            mp[prefix] = mp.get(prefix, 0) + 1
+
+        return res
+
+
