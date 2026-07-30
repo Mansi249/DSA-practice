e4c158ce32b2761dfba5c45fcd4614deb641d47a
Mansi249 <24bec062@nith.ac.in>
2026-07-30 20:35:33 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 19.3 MB (49.25%)

---
+++ leetcode-solutions/0289-game-of-life/solution.py
@@ -0,0 +1,24 @@
+class Solution:
+    def gameOfLife(self, board: List[List[int]]) -> None:
+        """
+        Do not return anything, modify board in-place instead.
+        """
+        rows,cols = len(board), len(board[0])
+        directions = [(-1,-1),(-1,0),(-1,1),(0,-1),(0,1),(1,-1),(1,0),(1,1)]
+        for r in range(rows):
+            for c in range(cols):
+                live_neighbors = 0
+                for dr,dc in directions:
+                    nr,nc = r+dr, c+dc
+                    if 0<= nr <rows and 0<=nc<cols and board[nr][nc] in (1,-1):
+                        live_neighbors +=1
+                if board[r][c] == 1 and (live_neighbors<2 or live_neighbors>3):
+                    board[r][c] = -1
+                elif board[r][c] == 0 and live_neighbors == 3:
+                    board[r][c] = 2
+        for r in range(rows):
+            for c in range(cols):
+                if board[r][c] == -1:
+                    board[r][c] = 0
+                elif board[r][c] == 2:
+                    board[r][c] = 1
