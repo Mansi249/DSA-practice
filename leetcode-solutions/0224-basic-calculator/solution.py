b02e2ca7a3bf416cf31b63568723cb331b95f4e8
Mansi249 <24bec062@nith.ac.in>
2026-08-03 14:15:18 +0000

    [LeetCode Sync] Runtime - 39 ms (53.82%), Memory - 20.1 MB (87.72%)

---
+++ leetcode-solutions/0224-basic-calculator/solution.py
@@ -0,0 +1,32 @@
+class Solution:
+    def calculate(self, s: str) -> int:
+        stack = []
+        result = 0
+        current_number = 0
+        sign = 1  
+
+        for char in s:
+            if char.isdigit():
+                current_number = current_number * 10 + int(char)
+                
+            elif char in "+-":
+                
+                result += sign * current_number
+                current_number = 0
+                
+                sign = 1 if char == '+' else -1
+                
+            elif char == '(':
+                stack.append(result)
+                stack.append(sign)
+                result = 0
+                sign = 1
+                
+            elif char == ')':
+                result += sign * current_number
+                current_number = 0
+                result *= stack.pop()  
+                result += stack.pop()  
+
+        return result + (sign * current_number)
+
