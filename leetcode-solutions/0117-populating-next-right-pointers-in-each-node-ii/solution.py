8cc3358c0a0011ad66d681f36bc3198f49558fbf
Mansi249 <24bec062@nith.ac.in>
2026-08-06 18:41:56 +0000

    [LeetCode Sync] Runtime - 47 ms (81.29%), Memory - 20.5 MB (25.99%)

---
+++ leetcode-solutions/0117-populating-next-right-pointers-in-each-node-ii/solution.py
@@ -0,0 +1,27 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val: int = 0, left: 'Node' = None, right: 'Node' = None, next: 'Node' = None):
+        self.val = val
+        self.left = left
+        self.right = right
+        self.next = next
+"""
+from collections import deque
+class Solution:
+    def connect(self, root: 'Node') -> 'Node':
+        if not root:
+            return root
+        queue = deque()
+        queue.append(root)
+        while queue:
+            level_size  = len(queue)
+            for i in range(level_size):
+                node = queue.popleft()
+                if i <level_size-1:
+                    node.next = queue[0]
+                else:
+                    node.next = None
+                if node.left : queue.append(node.left)
+                if node.right: queue.append(node.right)
+        return root
