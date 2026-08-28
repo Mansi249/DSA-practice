855c87bda019c54044796b3c7f0fc20474ffc28d
Mansi249 <24bec062@nith.ac.in>
2026-08-28 19:40:39 +0000

    [LeetCode Sync] Runtime - 79 ms (78.20%), Memory - 39.8 MB (35.11%)

---
+++ leetcode-solutions/0373-find-k-pairs-with-smallest-sums/solution.py
@@ -0,0 +1,24 @@
+class Solution:
+    def kSmallestPairs(self, nums1: List[int], nums2: List[int], k: int) -> List[List[int]]:
+        if not nums1 or not nums2 or k <= 0:
+            return []
+    
+        res = []
+    
+        min_heap = []
+    
+   
+        for i in range(min(len(nums1), k)):
+            current_sum = nums1[i] + nums2[0]
+            heapq.heappush(min_heap, (current_sum, i, 0))
+        
+    
+        while min_heap and len(res) < k:
+            curr_sum, i, j = heapq.heappop(min_heap)
+            res.append([nums1[i], nums2[j]])
+        
+            if j + 1 < len(nums2):
+                next_sum = nums1[i] + nums2[j + 1]
+                heapq.heappush(min_heap, (next_sum, i, j + 1))
+            
+        return res
