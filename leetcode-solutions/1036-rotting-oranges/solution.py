f079b57802f4f6bb9ed3deb7961919f7be50e168
Mansi249 <24bec062@nith.ac.in>
2026-09-01 21:03:36 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 19.3 MB (41.75%)

--- leetcode-solutions/1036-rotting-oranges/solution.py
+++ leetcode-solutions/1036-rotting-oranges/solution.py
@@ -1,27 +1,26 @@
-import collections
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
         rows = len(grid)
         cols = len(grid[0])
-        fresh = 0
-        q = collections.deque()
         time = 0
+        fresh = 0
+        queue = collections.deque()
         for r in range(rows):
             for c in range(cols):
-                if grid[r][c]== 1:
+                if grid[r][c] == 1:
                     fresh +=1
-                if grid[r][c] == 2:
-                    q.append((r,c))
-        directions = [(-1,0),(1,0),(0,-1),(0,1)]
-        while q and fresh >0 :
-            time +=1
-            for i in range(len(q)):
-                curr_r , curr_c = q.popleft()
+                elif grid[r][c] == 2:
+                    queue.append([r,c])
+        directions = [(1,0),(-1,0),(0,1),(0,-1)]
+        while queue and fresh>0 :
+            time+=1
+            for _ in range(len(queue)):
+                curr_r, curr_c = queue.popleft()
                 for dr,dc in directions:
-                    nr , nc = curr_r + dr , curr_c + dc
-                    if (0<=nr < rows) and (0 <= nc< cols) and (grid[nr][nc] == 1):
-                        fresh -=1
-                        grid[nr][nc] =2 
-                        q.append((nr,nc))
+                    sr,sc = curr_r+ dr, curr_c + dc
+                    if (0<=sr<rows) and (0<=sc< cols) and grid[sr][sc]==1:
+                        fresh-=1
+                        grid[sr][sc] = 2
+                        queue.append([sr,sc])
         return time if fresh ==0 else -1
 
