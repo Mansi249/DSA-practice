e40f6f7d56b0b858b4ce3bc4b0311be3b0e1d7f9
Mansi249 <24bec062@nith.ac.in>
2026-09-01 19:51:21 +0000

    [LeetCode Sync] Runtime - 230 ms (89.50%), Memory - 21.6 MB (48.21%)

--- leetcode-solutions/0200-number-of-islands/solution.py
+++ leetcode-solutions/0200-number-of-islands/solution.py
@@ -1,40 +1,19 @@
-import collections
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        if not grid:
-            return 0
-            
         rows = len(grid)
         cols = len(grid[0])
-        islands = 0
-        
-        
-        q = collections.deque()
-        
-        def bfs(r, c):
-            
-            
-            grid[r][c] = "0" 
-            directions = [(-1,0),(1,0),(0,-1),(0,1)]
-            
-            while q: 
-                curr_r, curr_c = q.popleft() 
-                
-                for dr, dc in directions:
-                    nr = curr_r + dr
-                    nc = curr_c + dc
-                    
-                    
-                    if (0 <= nr < rows) and (0 <= nc < cols) and (grid[nr][nc] == "1"):
-                        q.append((nr, nc))
-                        grid[nr][nc] = "0" 
-        
-        
+        island = 0
+        def dfs(r,c):
+            if r <0 or r >= rows or c>=cols or c<0 or grid[r][c] == "0":
+                return 
+            grid[r][c] = "0"
+            dfs(r+1,c)
+            dfs(r-1,c)
+            dfs(r,c+1)
+            dfs(r,c-1)
         for r in range(rows):
             for c in range(cols):
                 if grid[r][c] == "1":
-                    islands += 1
-                    q.append((r, c)) 
-                    bfs(r, c)        
-                    
-        return islands
+                    island+=1
+                    dfs(r,c)
+        return island
