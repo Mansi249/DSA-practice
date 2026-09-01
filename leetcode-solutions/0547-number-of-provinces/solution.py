f189e3c11068d384b5126e3602cfa00d15488423
Mansi249 <24bec062@nith.ac.in>
2026-09-01 19:38:51 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 21.7 MB (9.86%)

--- leetcode-solutions/0547-number-of-provinces/solution.py
+++ leetcode-solutions/0547-number-of-provinces/solution.py
@@ -2,15 +2,16 @@ class Solution:
     def findCircleNum(self, isConnected: List[List[int]]) -> int:
         n = len(isConnected)
         provinces = 0
+        visited = [0]*n
         def dfs(i):
             isConnected[i][i] = 0
             for j in range(n):
-                if isConnected[i][j] == 1:
+                if isConnected[i][j] == 1 and visited[j] == 0:
                     isConnected[i][j] = 0
-                    isConnected[j][i] = 0
+                    visited[j] = 1
                     dfs(j)
         for i in range(n):
-            if isConnected[i][i] ==1:
+            if visited[i] == 0:
                 provinces+=1
                 dfs(i)
-        return provinces
+        return provinces    
